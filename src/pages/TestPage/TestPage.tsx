93cd818f97cf4288948035da65fbdb877c18119f
liurrong <ypwenxiang@outlook.com>
2020-09-07 15:11:14 +0800

    add less and a test page

---
+++ src/pages/TestPage/TestPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TestPage.less";
+
+function TestPage(): JSX.Element {
+    return (
+        <div className="testpage">
+            hello testpage
+        </div>
+    )
+}
+
+export default TestPage;
