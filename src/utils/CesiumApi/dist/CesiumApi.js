cfc577d838129aa8f55505f80d808abd0bb5cc6d
liurrong <ypwenxiang@outlook.com>
2020-12-17 16:47:26 +0800

    cesium 各项基础操作

--- src/utils/CesiumApi/dist/CesiumApi.js
+++ src/utils/CesiumApi/dist/CesiumApi.js
@@ -1,18 +1,18 @@
-import * as Cesium from 'cesium';
-import "cesium/Build/Cesium/Widgets/widgets.css";
+"use strict";
+exports.__esModule = true;
+exports.changeHeight = exports.makeModelBaseLand = exports.addGeometry = exports.addBuilding = exports.setExtent = exports.initMap = void 0;
+var Cesium = require("cesium");
+require("cesium/Build/Cesium/Widgets/widgets.css");
 window.CESIUM_BASE_URL = './cesium/';
 Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3ZTIxYjQ0Yi1kODkwLTQwYTctYTdjNi1hOTkwYTRhYTI2NDEiLCJpZCI6MzY4OTQsImlhdCI6MTYwNDMwMzkzM30.btKZ2YlmB0wCTBvk3ewmGk5MAjS5rwl_Izra03VcrnY';
-
-const locationSZ = { lng: 114.167, lat: 22.67, height: 130000.0 };
+var locationSZ = { lng: 114.167, lat: 22.67, height: 130000.0 };
 // const locationJDY = { lng: 104.06, lat: 30.78, height: 13000.0 };
-const location = locationSZ;
-
+var location = locationSZ;
 // 初始化地图
-export const initMap = (domID: string, isAddBuilding: boolean) => {
-
-    if (!document.getElementById(domID)) return;
-
-    const viewer = new Cesium.Viewer(domID, {
+exports.initMap = function (domID, isAddBuilding) {
+    if (!document.getElementById(domID))
+        return;
+    var viewer = new Cesium.Viewer(domID, {
         geocoder: false,
         homeButton: true,
         sceneModePicker: false,
@@ -23,12 +23,11 @@ export const initMap = (domID: string, isAddBuilding: boolean) => {
         fullscreenButton: false,
         vrButton: false,
         terrainProvider: Cesium.createWorldTerrain()
-    })
+    });
     if (isAddBuilding) {
-        const tmpTileset = new Cesium.Cesium3DTileset({
+        var tmpTileset = new Cesium.Cesium3DTileset({
             url: "./Models/building/tileset.json"
-        })
-        // tmpTileset.mate
+        });
         tmpTileset.style = new Cesium.Cesium3DTileStyle({
             color: {
                 conditions: [
@@ -38,7 +37,7 @@ export const initMap = (domID: string, isAddBuilding: boolean) => {
                     ['${Height} >= 100', 'color("red")'],
                     ['true', 'color("blue")']
                 ]
-            },        
+            },
             // eslint-disable-next-line
             show: '${Height} > 0',
             meta: {
@@ -46,22 +45,14 @@ export const initMap = (domID: string, isAddBuilding: boolean) => {
                 description: '"Building id ${id} has height ${Height}."'
             }
         });
-
-
-
         viewer.scene.primitives.add(tmpTileset);
     }
-
-    // addGeometry(viewer, "no", { longitude: 123, latitude: 23, height: 23 });
-
-
-
-    setExtent(viewer);
+    exports.addGeometry(viewer, "no", { longitude: 123, latitude: 23, height: 23 });
+    exports.setExtent(viewer);
     return viewer;
-}
-
+};
 // 缩放到指定位置
-export const setExtent = (viewer: any) => {
+exports.setExtent = function (viewer) {
     viewer.camera.setView({
         destination: Cesium.Cartesian3.fromDegrees(location.lng, location.lat, location.height),
         orientation: {
@@ -70,41 +61,24 @@ export const setExtent = (viewer: any) => {
             roll: 0.0
         }
     });
-}
-
+};
 // 添加building
-export const addBuilding = (viewer: any, buildingUrl: string) => {
+exports.addBuilding = function (viewer, buildingUrl) {
     var entity = viewer.entities.add({
         name: "plane",
         position: Cesium.Cartesian3.fromDegrees(location.lng, location.lat, 1300.0),
         model: {
-            uri: "./Models/building.glb",
+            uri: "./Models/building.glb"
         }
     });
     //设置摄像头定位到模型处
     viewer.trackedEntity = entity;
-}
-
-
-// 添加几何体
-interface IPostion {
-    longitude: number,
-    latitude: number
-    height?: number
-}
-function computeCircle(radius: number) {
-    const positions = [];
-    for (let i = 0; i < 360; i++) {
-        const radians = Cesium.Math.toRadians(i);
-        positions.push(new Cesium.Cartesian2(radius * Math.cos(radians), radius * Math.sin(radians)));
-    }
-    return positions;
-}
-export const addGeometry = (viewer: any, type: string, postion: IPostion) => {
-    if (!viewer) return;
-
+};
+exports.addGeometry = function (viewer, type, postion) {
+    if (!viewer)
+        return;
     // 箱子纹理
-    const boxEntity = new Cesium.Entity({
+    var boxEntity = new Cesium.Entity({
         id: "boxID01",
         name: 'Red box with black outline',
         position: Cesium.Cartesian3.fromDegrees(113.91, 22.52, 100),
@@ -132,7 +106,6 @@ export const addGeometry = (viewer: any, type: string, postion: IPostion) => {
             //     offset: 20,
             //     orientation: Cesium.StripeOrientation.VERTICAL
             // }),
-
             // 网格纹理
             // material: new Cesium.GridMaterialProperty({
             //     color: Cesium.Color.YELLOW,
@@ -140,19 +113,17 @@ export const addGeometry = (viewer: any, type: string, postion: IPostion) => {
             //     lineCount: new Cesium.Cartesian2(8, 8),
             //     lineThickness: new Cesium.Cartesian2(2.0, 2.0)
             // }),
-
             // 渐变纹理
             material: new Cesium.ImageMaterialProperty({
                 image: getColorRamp([0.0, 0.045, 0.1, 0.15, 0.37, 0.54, 1.0], true),
-                transparent: true,
+                transparent: true
             }),
             outline: true,
             outlineColor: Cesium.Color.BLACK
         }
     });
-
     // 线纹理
-    const lineEntity = new Cesium.Entity({
+    var lineEntity = new Cesium.Entity({
         polyline: {
             positions: Cesium.Cartesian3.fromDegreesArray([113.90, 22.52,
                 113.91, 22.53]),
@@ -170,79 +141,36 @@ export const addGeometry = (viewer: any, type: string, postion: IPostion) => {
                 outlineColor: Cesium.Color.BLACK
             })
         }
-    })
-
-    // 动态纹理
-    const tubeEntity = new Cesium.Entity({
-        name: 'Red tube with rounded corners',
-        polylineVolume: {
-            positions: Cesium.Cartesian3.fromDegreesArray([113.90, 22.52, 113.904, 22.483]),
-            shape: computeCircle(60.0),
-            //颜色回调
-            material: new Cesium.ColorMaterialProperty(new Cesium.CallbackProperty(function () {
-                return Cesium.Color.fromRandom({
-                    minimumRed: 0.75,
-                    minimumGreen: 0.75,
-                    minimumBlue: 0.75,
-                    alpha: 1.0
-                });
-            }, false))
-        }
-    })
-    viewer.entities.add(tubeEntity);
-
-    // 贴地线1---修改声明文件
-    // const baseLine = new Cesium.Entity({
-    //     corridor: {
-    //         // positions: Cesium.Cartesian3.fromDegreesArray([113.90, 22.52,
-    //         //     113.91, 22.53]),
-    //         positions: Cesium.Cartesian3.fromDegreesArray([113.90, 22.52, 113.904, 22.483]),
-    //         width: 5,
-    //     }
-    // })
-    // viewer.entities.add(baseLine);
-
-    // 贴地线2--转为json文件--不成功
-    // viewer.dataSources.add(Cesium.GeoJsonDataSource.load('http://localhost:1234/lesson07/pwt1/line.json', {
-    //     clampToGround: true
-    // }));
-
-
-    // 贴地线3---成功
-    // viewer.scene.primitives.add(new Cesium.GroundPrimitive({
-    //     geometryInstances: new Cesium.GeometryInstance({
-    //         geometry: new Cesium.CorridorGeometry({
-    //             vertexFormat: Cesium.VertexFormat.POSITION_ONLY,
-    //             positions: Cesium.Cartesian3.fromDegreesArray([
-    //                 113.91, 22.52,
-    //                 113.904, 22.483                  
-    //             ]),
-    //             width: 10
-    //         }),
-    //         attributes: {
-    //             color: Cesium.ColorGeometryInstanceAttribute.fromColor(new Cesium.Color(0.0, 0.3, 0.9, 0.5))
-    //         }
-    //     }),
-    //     classificationType: Cesium.ClassificationType.TERRAIN
-    // }));
-
-
+    });
+    // 贴地线
+    viewer.scene.primitives.add(new Cesium.GroundPrimitive({
+        geometryInstances: new Cesium.GeometryInstance({
+            geometry: new Cesium.CorridorGeometry({
+                vertexFormat: Cesium.VertexFormat.POSITION_ONLY,
+                positions: Cesium.Cartesian3.fromDegreesArray([
+                    113.91, 22.52,
+                    113.904, 22.483
+                ]),
+                width: 10
+            }),
+            attributes: {
+                color: Cesium.ColorGeometryInstanceAttribute.fromColor(new Cesium.Color(0.0, 0.3, 0.9, 0.5))
+            }
+        }),
+        classificationType: Cesium.ClassificationType.TERRAIN
+    }));
     // 线纹理
     viewer.entities.add(boxEntity);
     viewer.entities.add(lineEntity);
- 
-
-}
-
+};
 // 获取颜色渐变条带
-const getColorRamp = (elevationRamp: any, isTransparent?: boolean) => {
-    const ramp = document.createElement('canvas');
+var getColorRamp = function (elevationRamp, isTransparent) {
+    var ramp = document.createElement('canvas');
     ramp.width = 1;
     ramp.height = 100;
-    const ctx: any = ramp.getContext('2d');
-
-    const values = elevationRamp;
-    const grd = ctx.createLinearGradient(0, 0, 0, 100);
+    var ctx = ramp.getContext('2d');
+    var values = elevationRamp;
+    var grd = ctx.createLinearGradient(0, 0, 0, 100);
     grd.addColorStop(values[0], '#000000'); //black
     grd.addColorStop(values[1], '#2747E0'); //blue
     grd.addColorStop(values[2], '#D33B7D'); //pink
@@ -251,30 +179,26 @@ const getColorRamp = (elevationRamp: any, isTransparent?: boolean) => {
     // grd.addColorStop(values[5], '#ffd700'); //yellow
     grd.addColorStop(values[5], 'transparent'); //yellow
     grd.addColorStop(values[6], '#ffffff'); //white
-
     ctx.fillStyle = grd;
     ctx.fillRect(0, 0, 1, 100);
     return ramp;
-}
-
-
+};
 // 调整位置，贴地
-export const makeModelBaseLand = (tileset: any) => {
+exports.makeModelBaseLand = function (tileset) {
     // 创建平移矩阵
-    const transition = Cesium.Cartesian3.fromArray([0, 0, 60]);
-    const m = Cesium.Matrix4.fromTranslation(transition);
+    var transition = Cesium.Cartesian3.fromArray([0, 0, 60]);
+    var m = Cesium.Matrix4.fromTranslation(transition);
     tileset._modelMatrix = m;
-}
-
+};
 // 调节高度
-export const changeHeight = (tileset: any, height: any) => {
+exports.changeHeight = function (tileset, height) {
     height = Number(height);
     if (isNaN(height)) {
         return;
     }
-    const cartographic = Cesium.Cartographic.fromCartesian(tileset.boundingSphere.center);
-    const surface = Cesium.Cartesian3.fromRadians(cartographic.longitude, cartographic.latitude, cartographic.height);
-    const offset = Cesium.Cartesian3.fromRadians(cartographic.longitude, cartographic.latitude, height);
-    const translation = Cesium.Cartesian3.subtract(offset, surface, new Cesium.Cartesian3());
+    var cartographic = Cesium.Cartographic.fromCartesian(tileset.boundingSphere.center);
+    var surface = Cesium.Cartesian3.fromRadians(cartographic.longitude, cartographic.latitude, cartographic.height);
+    var offset = Cesium.Cartesian3.fromRadians(cartographic.longitude, cartographic.latitude, height);
+    var translation = Cesium.Cartesian3.subtract(offset, surface, new Cesium.Cartesian3());
     tileset.modelMatrix = Cesium.Matrix4.fromTranslation(translation);
-}
+};
